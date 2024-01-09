8e7dcc4173b831772f46d76dfc0eb8ef2ed054c6
Alaicev <altarev123456@mail.ru>
2024-01-09 13:38:44 +0400

    feat: rename app add reload icon

--- app/src/main/java/com/devblok/kpc/tools/WebConstants.java
+++ app/src/main/java/com/devblok/kpc/tools/WebConstants.java
@@ -3,8 +3,8 @@ package com.devblok.kpc.tools;
 import okhttp3.MediaType;
 
 public class WebConstants {
-    public static final String backendUrl = "http://10.0.2.2:8080/api";
-    public static final String backendUrlBase = "http://10.0.2.2:8080";
+    public static final String backendUrl = "http://kpc-app.ru/api";
+    public static final String backendUrlBase = "http://kpc-app.ru";
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public static final String SHARED_PREFS = "shared_prefs";
